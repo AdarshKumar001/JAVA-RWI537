508a1b96cee7de14771cdecf9e535f6d1fd75eed
adarshKumar487 <167090501+adarshKumar487@users.noreply.github.com>
2024-04-16 14:35:50 +0530

    Add files via upload

---
+++ For_loop.java
@@ -0,0 +1,23 @@
+public class For_loop {
+    public static void main(String[]args){
+        for(int i=1; i<=100 ; i++){
+            if (i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+         //2nd example  print  pyramid
+
+//  public class  For_loop {
+//    public static void main(String[]args){
+//         for(int i=1;i<=5;i++){
+//               for(int j=1;j<=i;j++){
+//                System.out.print("* ");
+//            }
+//             System.out.println();
+//        }
+//    }
+//}

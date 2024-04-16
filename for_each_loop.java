d64ca21e022ad0112eca2e77bb7b2205c6ebfeb3
adarshKumar487 <167090501+adarshKumar487@users.noreply.github.com>
2024-04-16 14:35:14 +0530

    Add files via upload

---
+++ for_each_loop.java
@@ -0,0 +1,9 @@
+public class for_each_loop {
+    public static void main(String[]args){
+        int [] a={11,15,12,8,10};
+        for( int b: a){
+            System.out.println(b+  " ");
+        }
+ 
+     }
+}

7bed984540fa293488093d56d35c4095ca4b624b
Adarsh Kumar <167090501+AdarshKumar001@users.noreply.github.com>
2024-04-22 06:06:12 +0530

    Add files via upload

---
+++ String/Strings.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Strings {
+    // public static void main(String[] args) {
+    // String Name= "Adarsh";
+    // String Name2= "Adarsh Kumar";
+    // String Name3= "Adarsh Kumar Singh";
+    // System.out.println(Name);
+    // System.out.println(Name2);
+    // System.out.println(Name3);
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // String name= sc.nextLine();
+    // System.out.println("Your name is :"+ name);
+    // }
+
+    public static void main(String[] args) {
+    String firstName="Adarsh";
+    String lastName= "Kumar";
+    String fullName=firstName+" "+lastName;
+    System.out.println(fullName);
+    }
+}
+
+    

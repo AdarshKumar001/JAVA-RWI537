1754eb02a2759a5c60fe5f4ce9dedb305beeb0bf
Adarsh Kumar <167090501+AdarshKumar001@users.noreply.github.com>
2024-05-08 17:43:35 +0530

    Add files via upload

---
+++ Collections/Collection3.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Collection3 {
+    public static void main(String[] args) {
+        List<Integer> l = new ArrayList<>(); // Use generic type for ArrayList
+        l.add(10);
+        l.add(20);
+        l.add(30);
+        l.add(40);
+        System.out.println(l);
+
+        Iterator<Integer> itr = l.iterator(); // Use Iterator<Integer>
+        while(itr.hasNext()) { // Remove unintended semicolon
+            System.out.println(itr.next()); // Print each element in the list
+        }
+    }
+}

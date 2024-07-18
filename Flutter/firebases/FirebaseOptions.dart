7694686518b915e4996875332ee9aba07e5d3162
Adarsh Kumar <167090501+AdarshKumar001@users.noreply.github.com>
2024-07-18 11:19:55 +0530

    FirebaseOptions.dart

---
+++ Flutter/firebases/FirebaseOptions.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+const FirebaseOptions firebaseOptions = FirebaseOptions(
+  apiKey: 'AIzaSyCbIdB177_QHoXgvHpdGMElSGIeV28mUHY', // API key         //current_key": "AIzaSyCbIdB177_QHoXgvHpdGMElSGIeV28mUHY
+  appId: '1:47584980227:android:997d8aae6721bc25cc0d36', // App ID     //mobilesdk_app_id": "1:47584980227:android:997d8aae6721bc25cc0d36
+  messagingSenderId: '47584980227', // Messaging Sender ID            //project_number": "47584980227
+  projectId: 'fir-series-d8daf', // Project ID                       //project_id": "fir-series-d8daf
+  storageBucket: 'fir-series-d8daf.appspot.com', // Storage Bucket   //storage_bucket": "fir-series-d8daf.appspot.com
+);
+
+
+//google_services.json = file me se apiKey value,appId,
+// FirebaseOptions file bhi lib folder me hi banega main dart file k sath 
